53f5589453e07c1b0866d6fde0c43ca5c0d4e685
hanskim431 <95575606+hanskim431@users.noreply.github.com>
2024-07-01 23:01:46 +0900

    🎨 줄변경 수정

--- system/judge.py
+++ system/judge.py
@@ -15,13 +15,13 @@ def makeJudgment(user_charator:ch, cpu_charator:ch):
         if(user2.avoid_status == True): 
           return None
         else :
-          return f'{user1.name} 승리'
+          return f'\n{user1.name} 승리\n'
       
       if(user1.attack_point == ch.ATTACK):
         if(user2.avoid_status == True or user2.guard_status == True):
           return None
         else :
-          return f'{user1.name} 승리'
+          return f'\n{user1.name} 승리\n'
 
     (user1,user2) = (user2,user1)    
     
